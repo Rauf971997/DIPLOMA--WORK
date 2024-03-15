cf91255ed77c0dd7bb6453932ac036706217c709
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-15 15:05:52 +0400

    Test scroll effect for correct operation

--- my-app/src/HomeScreen.js
+++ my-app/src/HomeScreen.js
@@ -6,6 +6,21 @@ function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
+
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+
+      <h1>TEST</h1>
+      <h1>TEST</h1>
       {/* Banner */}
 
       {/* Row */}
