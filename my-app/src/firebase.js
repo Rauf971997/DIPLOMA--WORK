b495baa46ac711fce1e2a9ce68b11698557fcc5f
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-15 14:51:19 +0400

    Define and add initial settings for work with firebase

---
+++ my-app/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDEQVdrLj8BLdnL2g1yz-gCVNbglOHsHIg",
+    authDomain: "movie-app-bada1.firebaseapp.com",
+    projectId: "movie-app-bada1",
+    storageBucket: "movie-app-bada1.appspot.com",
+    messagingSenderId: "613871071893",
+    appId: "1:613871071893:web:676d2f61bfc4bc83fee19c"
+  };
