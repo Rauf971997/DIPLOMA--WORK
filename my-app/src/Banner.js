55d54ad2dc1410e2b552ba811914a1bf228a228f
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-15 15:42:14 +0400

    Define initial and testing structure for banner component

--- my-app/src/Banner.js
+++ my-app/src/Banner.js
@@ -8,7 +8,18 @@ function Banner(){
             backgroundImage:`url("https://wallpapercave.com/wp/wp1917118.jpg")`,
             backgroundPosition:"center center"
         }}>
-   
+         
+         <div className='banner__contents'>
+            <h1 className='banner__title'> Movie Name</h1>
+            <div className='banner__buttons'>
+                <button className='banner__button'>Play</button>
+                <button className='banner__button'>My List</button>
+            </div>
+
+            <h1 className='banner__description'>Test Description</h1>
+         </div>
+
+         <div className='banner--fadeBottom' />
         </header>
     )
 }
