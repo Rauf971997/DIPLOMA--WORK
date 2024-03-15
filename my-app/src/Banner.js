4a1f230d8787d0b3cff2f8b94c454acbe62dfdec
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-03-15 16:18:10 +0400

    Added a function to cut off the description if the number of words is more than 150

--- my-app/src/Banner.js
+++ my-app/src/Banner.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import './Banner.css';
+import React from "react";
+import "./Banner.css";
 
-function Banner(){
-    return (
-        <header className='banner' style={{
-            backgroundSize:"cover",
-            backgroundImage:`url("https://www.solidbackgrounds.com/images/1920x1080/1920x1080-black-solid-color-background.jpg")`,
-            backgroundPosition:"center center"
-        }}>
-         
-         <div className='banner__contents'>
-            <h1 className='banner__title'> Movie Name</h1>
-            <div className='banner__buttons'>
-                <button className='banner__button'>Play</button>
-                <button className='banner__button'>My List</button>
-            </div>
+function Banner() {
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
 
-            <h1 className='banner__description'>Test Description</h1>
-         </div>
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://www.solidbackgrounds.com/images/1920x1080/1920x1080-black-solid-color-background.jpg")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title"> Movie Name</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
 
-         <div className='banner--fadeBottom' />
-        </header>
-    )
+        <h1 className="banner__description">
+          {truncate('Test Description Test Description iopioiidfg ioiiopispodfisodfisdpofisdfisdfoisdfposidfposdifposdfispo jospdfpsodfispodifspo sdifposdifposdifpos sdifposdifposdifpos sdifposdifposdpof', 150)}
+            
+       </h1>
+      </div>
+
+      <div className="banner--fadeBottom" />
+    </header>
+  );
 }
-
 
 export default Banner;
